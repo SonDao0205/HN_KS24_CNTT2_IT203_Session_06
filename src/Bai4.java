65fb9930413ca323a8dbcc6b31edbfc725091278
Dao Truong Son <daotruongson2006@gmail.com>
2026-01-30 08:21:43 +0700

    submit

--- src/Bai4.java
+++ src/Bai4.java
@@ -17,11 +17,21 @@ public class Bai4 {
             this.employeeName = employeeName;
             this.salary = salary;
         }
+
+        public void showInfo(){
+            System.out.println("----------Employee----------");
+            System.out.println("Employee Id: "+employeeId);
+            System.out.println("Employee Name: "+employeeName);
+            System.out.println("Salary: "+salary);
+        }
     }
 
     public static void main(String[] args) {
         Employee em1 = new Employee();
         Employee em2 = new Employee("01","nguyen van a");
         Employee em3 = new Employee("02","nguyen van b",100);
+        em1.showInfo();
+        em2.showInfo();
+        em3.showInfo();
     }
 }
